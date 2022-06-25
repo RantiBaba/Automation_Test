94783749a4e5eabd9bd32e452c7cacdd62d82653
RantiBaba <64703698+RantiBaba@users.noreply.github.com>
2022-06-25 02:57:10 +0100

    add files to .gitinore

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,49 +1,37 @@
-describe("test", () => {
-  it("test", () => {
-    cy.visit(`http://automationpractice.com/index.php`, { timeout: 50000 });
-    cy.get(`[title="Dresses"]`, { timeout: 60000 })
-      .should(`be.visible`)
-      .eq(1)
-      .contains(`Dresses`)
-      .click({ force: true })
-      .then((clicked) => {
-        cy.get(`#list`).click();
-        cy.get(`.right-block-content.row`, { timeout: 60000 })
-          .as(`blocks`)
-          .find(`.price.product-price`)
-          .then(($prices) => {
-            const prices = Cypress._.map($prices, (price) =>
-              parseFloat(price.innerText.split(" ")[0].replace(/[^0-9.]/g, ""))
-            );
+context('Dress highest price', () => {
+  beforeEach(() => {
+    cy.visit(`/`)
+    cy.initial()
+  })
+  it('should add the highest price in dress price list to cart', () => {
+    cy.get('.sf-with-ul').contains(`Dresses`).click({ force: true })
+    cy.get(`#list`).click()
+    cy.get(`.right-block-content.row`)
+      .as(`right_block`)
+      .find(`.price.product-price`)
+      .then($prices => {
+        const prices = Cypress._.map($prices, price =>
+          parseFloat(price.innerText.split(' ')[0].replace(/[^0-9.]/g, ''))
+        )
 
-            cy.log(JSON.stringify(prices));
+        const max_price = Math.max(...prices)
 
-            const max_price = Math.max(...prices);
-
-            cy.get(`.right-block-content.row div .price.product-price`, {
-              timeout: 60000,
-            })
-              .contains(max_price)
-              .then((maximum) => {
-                cy.wrap(maximum)
-                  .parentsUntil(`div.right-block`)
-                  .find(`a[title="Add to cart"]`)
-                  .click();
-                cy.get(`#layer_cart`, { timeout: 10000 })
-                  .find(`h2`)
-                  .should(
-                    `contain.text`,
-                    `Product successfully added to your shopping cart`
-                  );
-                cy.get(`#layer_cart_product_price`).should(
-                  `contain`,
-                  max_price
-                );
-                cy.get(`[title="Close window"]`, { timeout: 10000 }).click();
-              });
-          });
-      });
-  });
-
-  
-});
+        cy.get(`.right-block-content.row div .price.product-price`)
+          .contains(max_price)
+          .then(maximum => {
+            cy.wrap(maximum)
+              .parentsUntil(`div.right-block`)
+              .find(`a[title="Add to cart"]`)
+              .click()
+            cy.get(`#layer_cart`)
+              .find(`h2`)
+              .should(
+                `contain.text`,
+                `Product successfully added to your shopping cart`
+              )
+            cy.get(`#layer_cart_product_price`).should(`contain`, max_price)
+            cy.get(`[title="Close window"]`).click()
+          })
+      })
+  })
+})
